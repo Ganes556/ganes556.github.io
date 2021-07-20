28bb93f8aa2186a969eb4b70c0f3fa34ee36c4a3
ganes <gustiganes@gmail.com>
2021-07-20 14:05:27 +0800

    aos lib added

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 function open_animation() {
   document.querySelector(".animation-x").classList.toggle("open");
 }
+// link_click and add the active class
+const link_menu = document.querySelectorAll(".nav-link");
+link_menu.forEach((e) => {
+  e.addEventListener("click", () => {
+    link_menu.forEach((d) => {
+      d.classList.remove("active");
+    });
+    e.classList.add("active");
+  });
+});
