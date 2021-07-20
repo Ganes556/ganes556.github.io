4f93e18c2fc3c69d6da10a4438460575d713b542
ganes <gustiganes@gmail.com>
2021-07-20 15:36:30 +0800

    scroll spy

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-function open_animation() {
-  document.querySelector(".animation-x").classList.toggle("open");
-}
-// link_click and add the active class
-const link_menu = document.querySelectorAll(".nav-link");
-link_menu.forEach((e) => {
-  e.addEventListener("click", () => {
-    link_menu.forEach((d) => {
-      d.classList.remove("active");
-    });
-    e.classList.add("active");
-  });
+const navbarToggler = document.querySelector("button.navbar-toggler");
+const spanToggler = document.querySelector(".animation-x");
+navbarToggler.addEventListener("click", () => {
+  spanToggler.classList.toggle("open");
 });
+
+// link_click and add the active class
+// const link_menu = document.querySelectorAll(".nav-link");
+// link_menu.forEach((e) => {
+//   e.addEventListener("click", () => {
+//     link_menu.forEach((d) => {
+//       d.classList.remove("active");
+//     });
+//     e.classList.add("active");
+//   });
+// });
