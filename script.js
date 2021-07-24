2da1e9a00723c6ee25c5fb87638699a0bc68c413
ganes <gustiganes@gmail.com>
2021-07-24 13:13:39 +0800

    fixed

--- script.js
+++ script.js
@@ -19,12 +19,12 @@ $(document).ready(function () {
   let link_selector = ".nav-link";
   let link_page = "section";
   $(link_selector).removeClass("active");
-  // if (location.hash === "") {
-  //   $("a[href='#home']").addClass("active");
-  // } else {
-  // }
-  location.hash = localStorage["current-link"];
-  $("a[href='" + location.hash + "']").addClass("active");
+  if (location.hash !== "") {
+    location.hash = localStorage["current-link"];
+    $("a[href='" + location.hash + "']").addClass("active");
+  } else {
+    $("a[href='#home']").addClass("active");
+  }
 
   $(window).scroll(function () {
     let top = $(window).scrollTop();
