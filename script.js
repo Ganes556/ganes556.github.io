9fd9e4729004eb480bc1eff330d4cc67beeab854
ganes <gustiganes@gmail.com>
2021-08-08 12:53:08 +0800

    login system with js

---
+++ script.js
@@ -0,0 +1,11 @@
+let login = {
+  name: "GanesSatsangga",
+  password: "GanesSatsangga",
+};
+$(document).ready(function () {
+  $(".btn").click(function (e) {
+    e.preventDefault();
+    console.log(login);
+    // $("#floatingUsernameInput").val();
+  });
+});
